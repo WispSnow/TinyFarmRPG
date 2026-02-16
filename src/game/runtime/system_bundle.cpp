cb52c5ed57f664b907ee00bd35c1a7abe909e514
Ziyu <theorhythm@gmail.com>
2026-02-16 23:30:46 +0800

    Add runtime assembly to GameScene and update CMake configuration

---
+++ src/game/runtime/system_bundle.cpp
@@ -0,0 +1,56 @@
+#include "system_bundle.h"
+
+#include "engine/system/render_system.h"
+#include "engine/system/light_system.h"
+#include "engine/system/ysort_system.h"
+#include "engine/system/debug_render_system.h"
+#include "engine/system/movement_system.h"
+#include "engine/system/animation_system.h"
+#include "engine/system/spatial_index_system.h"
+#include "engine/system/auto_tile_system.h"
+#include "engine/system/remove_entity_system.h"
+#include "engine/system/audio_system.h"
+#include "engine/spatial/collision_resolver.h"
+#include "game/system/state_system.h"
+#include "game/system/action_sound_system.h"
+#include "game/system/player_control_system.h"
+#include "game/system/camera_follow_system.h"
+#include "game/system/farm_system.h"
+#include "game/system/pickup_system.h"
+#include "game/system/render_target_system.h"
+#include "game/system/animation_event_system.h"
+#include "game/system/time_system.h"
+#include "game/system/time_of_day_light_system.h"
+#include "game/system/light_toggle_system.h"
+#include "game/system/day_night_system.h"
+#include "game/system/crop_system.h"
+#include "game/system/inventory_system.h"
+#include "game/system/hotbar_system.h"
+#include "game/system/item_use_system.h"
+#include "game/system/npc_wander_system.h"
+#include "game/system/animal_behavior_system.h"
+#include "game/system/dialogue_system.h"
+#include "game/system/chest_system.h"
+#include "game/system/interaction_system.h"
+#include "game/system/rest_system.h"
+#include "game/system/map_transition_system.h"
+#include "game/factory/entity_factory.h"
+#include "game/factory/blueprint_manager.h"
+#include "game/data/item_catalog.h"
+#include "game/save/save_service.h"
+#include "game/world/world_state.h"
+#include "game/world/map_manager.h"
+
+namespace game::runtime {
+
+GameRuntimeServices::GameRuntimeServices() = default;
+GameRuntimeServices::~GameRuntimeServices() noexcept = default;
+GameRuntimeServices::GameRuntimeServices(GameRuntimeServices&&) noexcept = default;
+GameRuntimeServices& GameRuntimeServices::operator=(GameRuntimeServices&&) noexcept = default;
+
+GameSystemBundle::GameSystemBundle() = default;
+GameSystemBundle::~GameSystemBundle() noexcept = default;
+GameSystemBundle::GameSystemBundle(GameSystemBundle&&) noexcept = default;
+GameSystemBundle& GameSystemBundle::operator=(GameSystemBundle&&) noexcept = default;
+
+} // namespace game::runtime
---
+++ src/game/runtime/system_bundle.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "engine/system/fwd.h"
+#include "game/system/fwd.h"
+
+#include <memory>
+
+namespace engine::spatial {
+class CollisionResolver;
+}
+
+namespace game::factory {
+class BlueprintManager;
+class EntityFactory;
+}
+
+namespace game::data {
+class ItemCatalog;
+}
+
+namespace game::save {
+class SaveService;
+}
+
+namespace game::world {
+class WorldState;
+class MapManager;
+}
+
+namespace game::runtime {
+
+struct GameRuntimeServices {
+    std::shared_ptr<game::factory::BlueprintManager> blueprint_manager;
+    std::shared_ptr<game::data::ItemCatalog> item_catalog;
+
+    std::unique_ptr<engine::spatial::CollisionResolver> collision_resolver;
+    std::unique_ptr<game::world::WorldState> world_state;
+    std::unique_ptr<game::factory::EntityFactory> entity_factory;
+    std::unique_ptr<game::world::MapManager> map_manager;
+    std::unique_ptr<game::save::SaveService> save_service;
+
+    GameRuntimeServices();
+    ~GameRuntimeServices() noexcept;
+    GameRuntimeServices(GameRuntimeServices&&) noexcept;
+    GameRuntimeServices& operator=(GameRuntimeServices&&) noexcept;
+
+    GameRuntimeServices(const GameRuntimeServices&) = delete;
+    GameRuntimeServices& operator=(const GameRuntimeServices&) = delete;
+};
+
+struct GameSystemBundle {
+    std::unique_ptr<engine::system::RenderSystem> render_system;
+    std::unique_ptr<engine::system::LightSystem> light_system;
+    std::unique_ptr<engine::system::YSortSystem> ysort_system;
+#ifdef TF_ENABLE_DEBUG_UI
+    std::unique_ptr<engine::system::DebugRenderSystem> debug_render_system;
+#endif
+    std::unique_ptr<engine::system::MovementSystem> movement_system;
+    std::unique_ptr<engine::system::AnimationSystem> animation_system;
+    std::unique_ptr<engine::system::SpatialIndexSystem> spatial_index_system;
+    std::unique_ptr<engine::system::AutoTileSystem> auto_tile_system;
+    std::unique_ptr<engine::system::RemoveEntitySystem> remove_entity_system;
+    std::unique_ptr<engine::system::AudioSystem> audio_system;
+
+    std::unique_ptr<game::system::StateSystem> state_system;
+    std::unique_ptr<game::system::ActionSoundSystem> action_sound_system;
+    std::unique_ptr<game::system::PlayerControlSystem> player_control_system;
+    std::unique_ptr<game::system::CameraFollowSystem> camera_follow_system;
+    std::unique_ptr<game::system::FarmSystem> farm_system;
+    std::unique_ptr<game::system::PickupSystem> pickup_system;
+    std::unique_ptr<game::system::RenderTargetSystem> render_target_system;
+    std::unique_ptr<game::system::AnimationEventSystem> animation_event_system;
+    std::unique_ptr<game::system::TimeSystem> time_system;
+    std::unique_ptr<game::system::TimeOfDayLightSystem> time_of_day_light_system;
+    std::unique_ptr<game::system::LightToggleSystem> light_toggle_system;
+    std::unique_ptr<game::system::DayNightSystem> day_night_system;
+    std::unique_ptr<game::system::CropSystem> crop_system;
+    std::unique_ptr<game::system::InventorySystem> inventory_system;
+    std::unique_ptr<game::system::HotbarSystem> hotbar_system;
+    std::unique_ptr<game::system::ItemUseSystem> item_use_system;
+    std::unique_ptr<game::system::NPCWanderSystem> npc_wander_system;
+    std::unique_ptr<game::system::AnimalBehaviorSystem> animal_behavior_system;
+    std::unique_ptr<game::system::DialogueSystem> dialogue_system;
+    std::unique_ptr<game::system::ChestSystem> chest_system;
+    std::unique_ptr<game::system::InteractionSystem> interaction_system;
+    std::unique_ptr<game::system::RestSystem> rest_system;
+    std::unique_ptr<game::system::MapTransitionSystem> map_transition_system;
+
+    GameSystemBundle();
+    ~GameSystemBundle() noexcept;
+    GameSystemBundle(GameSystemBundle&&) noexcept;
+    GameSystemBundle& operator=(GameSystemBundle&&) noexcept;
+
+    GameSystemBundle(const GameSystemBundle&) = delete;
+    GameSystemBundle& operator=(const GameSystemBundle&) = delete;
+};
+
+} // namespace game::runtime
