201e146ff076411d7a77b2a473432b60770a80fb
Ziyu <theorhythm@gmail.com>
2026-05-23 09:00:04 -0400

    优化战斗场景中角色姓名显示

--- tests/game/rmlui_architecture_regression_test.cpp
+++ tests/game/rmlui_architecture_regression_test.cpp
@@ -157,7 +157,7 @@ TEST(RmlUiArchitectureRegressionTest, BattleSceneUsesPlainButtonsAndDivBars) {
     EXPECT_EQ(rml.find("<progress"), std::string::npos);
     EXPECT_NE(rml.find("battle-text-button"), std::string::npos);
     EXPECT_NE(rml.find("id=\"battle-top-status\""), std::string::npos);
-    EXPECT_NE(rml.find("battle-party-identity"), std::string::npos);
+    EXPECT_NE(rml.find("battle-party-body"), std::string::npos);
     EXPECT_NE(rml.find("data-style-decorator=\"member.portrait_decorator\""), std::string::npos);
     EXPECT_NE(rml.find("data-style-width=\"member.hp_ratio_percent\""), std::string::npos);
     EXPECT_NE(rml.find("data-style-width=\"member.mp_ratio_percent\""), std::string::npos);
